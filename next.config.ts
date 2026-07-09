e7118f32375d26ea052e4efa87d436cf9bc2893c
witch661 <15895649968@163.com>
2026-07-09 18:27:50 +0800

    fix config for vercel

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: 'export',
-  distDir: 'dist',
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
