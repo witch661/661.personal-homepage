bd900ef9b3673bc487faec4a9a0afa32e8b53af7
witch661 <15895649968@163.com>
2026-07-09 19:30:49 +0800

    static export for github pages

--- dist/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"5YVcZzkBkGv3wpLVyuGaX9zBwzuFBKNaI8UqxBlOMjY=\"\n}"
--- dist/dev/static/chunks/app_page_tsx_1-7dtlk._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/app_page_tsx_1s_43kl._.js","static/chunks/node_modules_1uk-o7a._.js"],
-    source: "dynamic"
-});
